cc8242bc0bb0cb7754b91f3a825dd8616d29f985
nagarajuqatrainer <63718824+nagarajuqatrainer@users.noreply.github.com>
2021-03-10 19:12:42 +0530

    First Commit

---
+++ com.app.TestProject/src/main/java/com/app/Utilities/App.java
@@ -0,0 +1,12 @@
+package com.app.Utilities;
+
+
+public class App 
+{
+    
+	
+	
+	
+	
+	
+}
---
+++ com.app.TestProject/src/test/java/com/app/TestCases/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.TestCases;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+   
+    @Test
+    public void verifytest()
+    {
+     
+    	//How to run this project using jenkins
+    	
+    	System.out.println("Selenium Test");
+    	System.out.println("Java Selenium");
+    	System.out.println("Python test");
+    	System.out.println("Ruby Test");
+    	
+    }
+}
